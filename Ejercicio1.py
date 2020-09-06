f91708f688fa425e08e5a6dfb422c488e1a87e61
Natalia <montenegro.natismontenegro@hotmail.com>
2020-09-06 15:54:50 -0500

    Inicializacion entrada de datos Ejercicio 1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,3 +1,4 @@
 print ("Bienvenido al programa 1")
 print ("Este programa está diseñado para verificar si un número es par o impar")
 print ("Let's do it!")
+print ("Digite un número positivo de su preferencia")
