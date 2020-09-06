b58d173a182cc5c11d8125aeffc47cca503533dd
Natalia <montenegro.natismontenegro@hotmail.com>
2020-09-06 17:20:58 -0500

    Inicializacion entrada de datos

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,3 +1,6 @@
 print ("Bienvenido al Ejercicio 2")
 print ("Este programa está diseñado para que el usuario pueda calcular su edad en el año 2070 ")
 print ("Let´s do it!")
+
+edad = (int(input("Ingrese su edad actual")))
+año = (int(input("Ingrese el año actual")))
