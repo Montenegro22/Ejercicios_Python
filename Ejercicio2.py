86bf71da681f6a332e4050cadcb8c89f3e68a6db
Natalia <montenegro.natismontenegro@hotmail.com>
2020-09-06 17:28:54 -0500

    Se termina el procedimiento solicitado

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -2,5 +2,10 @@ print ("Bienvenido al Ejercicio 2")
 print ("Este programa está diseñado para que el usuario pueda calcular su edad en el año 2070 ")
 print ("Let´s do it!")
 
-edad = (int(input("Ingrese su edad actual")))
-año = (int(input("Ingrese el año actual")))
+edad = (int(input("Ingrese su edad actual -> ")))
+año = (int(input("Ingrese el año actual -> ")))
+
+resultado = (2070-año)
+resultado_final = (resultado+edad)
+
+print ("Su edad en el año 2070 serán " +resultado_final+ "años")
