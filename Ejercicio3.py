ec3f6cfa8060b3e6a1eeae83d58ae9d94bb1a353
Natalia <montenegro.natismontenegro@hotmail.com>
2020-09-06 17:52:19 -0500

    Explicacion requerimiento de dias Ejercicio 3

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -5,3 +5,4 @@ print ("Let´s do it!")
 input("Digite el nombre del producto que desea comprar -> ")
 int(input("Digite el valor del producto que desea comprar -> "))
 
+print ("A continuación deberá digitar el día de la semana para hacer efectivo el descuento de la siguiente manera: Si es lunes digitar ""l"", si es martes digitar ""m"", miercoles ""M"", jueves ""j"", viernes ""v"", sabado ""s"", domingo ""d"" respectivamente  "
