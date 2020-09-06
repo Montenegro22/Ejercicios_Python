ae1881acf39d8ced776247fb43ccf919288de5c9
Natalia <montenegro.natismontenegro@hotmail.com>
2020-09-06 17:41:42 -0500

    Inicializacion Ejercicio 3

---
+++ Ejercicio3.py
@@ -0,0 +1,3 @@
+print ("Bienvenido al Ejercicio 3")
+print ("Este programa está diseñado para que el usuario pueda calcular el descuento de un producto dependiendo el día de la semana ")
+print ("Let´s do it!")
