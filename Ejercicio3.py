ef847a81383fea3a51bdbc88e91a446db5036714
Natalia <montenegro.natismontenegro@hotmail.com>
2020-09-06 17:48:46 -0500

    Inicializacion saludo y datos de entrada Ejercicio 3

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,3 +1,7 @@
 print ("Bienvenido al Ejercicio 3")
 print ("Este programa está diseñado para que el usuario pueda calcular el descuento de un producto dependiendo el día de la semana ")
 print ("Let´s do it!")
+
+input("Digite el nombre del producto que desea comprar -> ")
+int(input("Digite el valor del producto que desea comprar -> "))
+
